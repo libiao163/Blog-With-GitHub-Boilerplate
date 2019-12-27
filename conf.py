a3e059341f68278270cb3e80a6339ef3d1256f83
libiao163 <libiao163@gmail.com>
2019-12-27 14:54:33 +0800

    init blog

--- conf.py
+++ conf.py
@@ -9,30 +9,30 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "libiao163/libiao163.github.io@master"
 }
 
 # 站点设置
-site_name = "的个人博客"
+site_name = "悄悄生长"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+site_build_date = "2019-12-25T18:27+08:00"
+author = "酒后的阿bill"
+email = "libiao163@gmail.com"
+author_homepage = "https://libiao163.github.io/"
+description = "认真是我们参与这个社会的方式，认真是我们改变这个社会的方式"
+key_words = ['Maverick', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
-        "name": "Maverick",
-        "url": "https://github.com/AlanDecode/Maverick",
-        "brief": "🏄‍ Go My Own Way."
+        "name": "coolshell",
+        "url": "https://coolshell.cn/",
+        "brief": "陈皓"
     },
     {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
+        "name": "ruanyf"
+        "url": "http://www.ruanyifeng.com/"
+        "brief": "阮一峰"
     }
 ]
 nav = [
@@ -56,17 +56,17 @@ nav = [
 social_links = [
     {
         "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
+        "url": "https://twitter.com/",
         "icon": "gi gi-twitter"
     },
     {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/libiao163",
         "icon": "gi gi-github"
     },
     {
         "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
+        "url": "https://weibo.com/6462830524/",
         "icon": "gi gi-weibo"
     }
 ]
